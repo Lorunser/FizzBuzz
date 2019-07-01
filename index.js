b5786e2739754c17c01b1d85e2d8736b350a4618
Lorunser <ltray99@gmail.com>
2019-07-01 17:28:56 +0100

    Complete extensions

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-function Convert(num){
-    var message = "";
+//imports
+const readline = require('readline-sync');
 
-    if(num % 3 == 0){
-        message = message + "Fizz";
+function Convert(num, rules){
+    var message = new Array();
+
+    //Fizz - 3
+    if(num % 3 == 0 && rules.indexOf('3') != -1){
+        message.push("Fizz");
     }
     
-    if(num % 5 == 0){
-        message = message + "Buzz";
+    //Buzz - 5
+    if(num % 5 == 0 && rules.indexOf('5') != -1){
+        message.push("Buzz");
     }
 
-    if(num % 7 == 0){
-        message = message + "Bang"; 
+    //Bang - 7
+    if(num % 7 == 0 && rules.indexOf('7') != -1){
+        message.push("Bang"); 
+    }
+
+    //Bong - 11
+    if(num % 11 == 0 && rules.indexOf('11') != -1){
+        //clear and add bong
+        message = new Array()
+        message.push("Bong")
+    }
+
+    //Fezz - 13
+    if(num % 13 == 0 && rules.indexOf('13') != -1){
+        //insert a Fezz before any B's
+        var firstBIndex = -1;
+
+        for(var i = 0; i < message.length; i++){
+            var item = message[i];
+            var firstLetter = item[0]
+
+            if (firstLetter == "B"){17
+                firstBIndex = i;
+                break;
+            }
+        }
+
+        message.splice(firstBIndex, 0, "Fezz");
+    }
+
+    //Reverse - 17
+    if(num % 17 && rules.indexOf('17') != -1){
+        message.reverse();
     }
     
-
     //default case
-    if(message == ""){
-        message = String(num)
+    if(message.length == 0){
+        message = String(num);
+    }
+
+    else
+    {
+        message = message.reduce(function(value, accumulator){return value + accumulator;});
     }
 
     return message;
 }
 
 
-for(var i = 1; i <= 100; i++){
-    var message = Convert(i);
-    console.log(message);
+function main(){
+    console.log('Number to count up to: ')
+    var max_num = readline.prompt();
+
+    console.log('Rules to allow: 3 5 7 11 13 17 (separate with spaces)')
+    var rules = readline.prompt();
+    rules = rules.split(' ');
+    
+    for(var i = 1; i <= max_num; i++){
+        var message = Convert(i, rules);
+        console.log(message);
+    }
 }
+
+
+main();
